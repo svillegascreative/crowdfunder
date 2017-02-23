82081cd64c80a41a8e5de3072b998ff8fa859a21
sylvia villegas <sylvia@svillegas.com>
2017-02-23 17:04:17 -0500

    Make project creator owner of the project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -18,6 +18,7 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(project_params)
+    @project.user_id = current_user.id
 
     if @project.save
       redirect_to projects_url

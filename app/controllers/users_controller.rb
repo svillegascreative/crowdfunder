a7903c51e3b453a6f9c654b3474ab22e0443ad52
Paul Walsh <twentytwentyk@live.com>
2017-02-24 15:36:25 -0500

    redirect to user page login and create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       auto_login(@user)
-      redirect_to projects_url
+      redirect_to(@user)
     else
       render 'new'
     end
